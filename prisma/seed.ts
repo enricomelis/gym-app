52427aa0c8d3360b8f78745f047a3cac97d6b6c3
Enrico <enrico.melis.casa@gmail.com>
2026-03-08 19:03:38 +0100

    created demo for prisma schema

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const prisma = new PrismaClient({
+	adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+});
+
+async function main() {
+	await prisma.user.deleteMany();
+
+	await prisma.user.createMany({
+		data: [
+			{
+				email: "ghilarduccidavid@gmail.com",
+				password: "hashed_placeholder_1",
+				role: "TECNICO",
+			},
+			{
+				email: "enrico.melis.casa@gmail.com",
+				password: "hashed_placeholder_2",
+				role: "ATLETA",
+			},
+		],
+	});
+
+	console.log("Seeded 2 users");
+}
+
+main()
+	.then(() => prisma.$disconnect())
+	.catch((e) => {
+		console.error(e);
+		prisma.$disconnect();
+		process.exit(1);
+	});
